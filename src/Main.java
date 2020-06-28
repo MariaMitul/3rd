fd9e52fd0373690e24cea75306472d7626ab2d2e
Maria Mitul <mitulmaria@gmail.com>
2020-06-28 23:21:24 +0300

    deleted the last 3 lines

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sashulea");
         System.out.println("Mashulea");
-        System.out.println("inimioara");
-        System.out.println("puisori");
-        System.out.println("mi-i somn");
     }
 }
