d673b9a322f1fdf1e6706d41fe993cda94c1c704
Maria Mitul <mitulmaria@gmail.com>
2020-06-28 23:22:20 +0300

    added pupsicei line

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sashulea");
         System.out.println("Mashulea");
+        System.out.println("pupsicei");
     }
 }
